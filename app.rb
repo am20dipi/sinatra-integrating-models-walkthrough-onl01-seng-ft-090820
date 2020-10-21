82c19ddfbf7e876fa3d47dcfabccfc177cbcbdca
am20dipi <am20dipi@alum.siena.edu>
2020-10-21 14:59:23 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-require_relative 'models/text_analyzer.rb'
+require_relative 'models/text_analyzer.rb' #adds the ability to reference the TextAnalyzer class and invokes its "new" method
 
 class App < Sinatra::Base
   get '/' do
@@ -8,7 +8,10 @@ class App < Sinatra::Base
 
   post '/' do
     text_from_user = params[:user_text]
+    @analyzed_text = TextAnalyzer.new(text_from_user)
 
     erb :results
+    #takes the data from params[:user_text] and feeds it into a new instance of the TextAnalyzer class
+    #we now have the instance of TextAnalyzer saved to an instance var called '@analyzed_text"
   end
 end
